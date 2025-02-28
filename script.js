0ea1a0ed44a52253f2b2e497ae6e9fc7eb104a17
SajeebSanto <sajeeb.santo@enosisbd.com>
2025-02-28 19:57:08 +0600

    Implemented all the features

--- script.js
+++ script.js
@@ -6,12 +6,7 @@ let movieData = [
     ["The Lord of the Rings: The Return of the King", "Fantasy", "2003-12-17", 5]
 ];// Initial movie data
 
-console.log(movieData);
-
-
-
-let gridInstance = null;
-let count = 0; // Grid.js instance
+let gridInstance = null; // Grid.js instance
 const renderGrid = () => {
     const gridContainer = document.getElementById("grid");
     gridContainer.innerHTML = ""; // Clear the container
@@ -28,8 +23,8 @@ const renderGrid = () => {
                     formatter: (_, row) => {
                         console.log(typeof(row.cells[0].data));
                         return gridjs.html(`
-                            <button class = "btn btn-edit" onclick="editMovie('${row.cells[0].data}');">Edit</button>
-                            <button class = "btn btn-delete" onclick="deleteMovie('${row.cells[0].data}');">Delete</button>
+                            <button class = "btn btn-edit" onclick="editMovie('${row.cells[0].data}', '${row.cells[1].data}', '${row.cells[2].data}', ${row.cells[3].data});">Edit</button>
+                            <button class = "btn btn-delete" onclick="deleteMovie('${row.cells[0].data}', '${row.cells[1].data}', '${row.cells[2].data}', ${row.cells[3].data});">Delete</button>
                         `);
                     }
                 }
@@ -59,14 +54,46 @@ function toggleForm() {
     document.getElementById("listContainer").classList.toggle("active");
 }
 
+//Function to edit a movie
+let editIndex = -1;
+function editMovie(name, genre, releaseDate, rating) {
+    document.getElementById("title").value = name;
+    document.getElementById("genre").value = genre;
+    document.getElementById("releaseDate").value = releaseDate;
+    document.getElementById("rating").value = rating;
+    editIndex = movieData.findIndex((movie) => {
+        return (
+            movie[0] === name &&
+            movie[1] === genre &&
+            movie[2] === releaseDate &&
+            movie[3] === rating
+        )
+    });
+    toggleForm();
+}
+
 // Function to add a new movie
 function submitMovie() {
     const title = document.getElementById("title").value;
     const genre = document.getElementById("genre").value;
     const releaseDate = document.getElementById("releaseDate").value;
     const rating = document.getElementById("rating").value;
-    movieData.push([title, genre, releaseDate, rating]);
-    console.log("Moviedata length after submit: ", movieData.length);
+
+    //Field Validations
+    if (!title || !genre || !releaseDate || !rating) {
+        alert("Please fill in all the fields.");
+        return;
+    }
+    if(isNaN(rating) || rating < 1 || rating > 5) {
+        alert("Rating should be a number between 1 and 5.");
+        return;
+    }
+
+    if(editIndex !== -1) {
+        movieData[editIndex] = [title, genre, releaseDate, rating];
+        editIndex = -1;
+    }
+    else movieData.push([title, genre, releaseDate, rating]);
     renderGrid();
     toggleForm();
     // Reset the form values
@@ -74,15 +101,17 @@ function submitMovie() {
 }
 
 // Function to delete a movie
-function deleteMovie(movieName) {
-    //console.log("Movie Name: ", movieName);
-    let index = movieData.findIndex((movie) => movie[0] === movieName);
-    console.log("Index: ", index);
+function deleteMovie(movieName, movieGenre, movieReleaseDate, movieRating) {
+    let index = movieData.findIndex((movie) => {
+        return (
+            movie[0] === movieName &&
+            movie[1] === movieGenre &&
+            movie[2] === movieReleaseDate &&
+            movie[3] === movieRating
+        )
+    });
     movieData.splice(index, 1);
     renderGrid();
 }
 
-//Function to edit a movie
-function editMovie(movie) {
-    
-}
+
